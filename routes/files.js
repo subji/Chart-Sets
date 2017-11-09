680ec670eb0657509747796241356dab11054fee
unknown <qkrwlrjq@gmail.com>
2017-11-09 09:53:57 +0900

    'maplot'

--- routes/files.js
+++ routes/files.js
@@ -12,30 +12,4 @@ router.get('/maplot', function(req, res, next) {
   });
 });
 
-router.get('/xyplot', function(req, res, next) {
-	var data = null;
-
-  fs.readFile('/data/public/node/ercsbplot/public/datas/xyplot.json', 'utf8', function (err, d)	{
-  	res.json(JSON.parse(d));
-  });
-});
-
-router.get('/degplot', function(req, res, next) {
-	var data = null;
-
-  fs.readFile('/data/public/node/ercsbplot/public/datas/degplot.json', 'utf8', function (err, d)	{
-  	res.json(JSON.parse(d));
-  });
-});
-
-router.get('/pcaplot', function(req, res, next) {
-  var data = null;
-
-  fs.readFile('/data/public/node/ercsbplot/public/datas/PCA.dat.tsv', 'utf8', function (err, d)  {
-    // File data 를 보내고자 할 때는 이렇게 보내자.
-    res.send(d);
-  });
-});
-
-
 module.exports = router;
